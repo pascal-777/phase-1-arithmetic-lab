09e58b230e0ca28f8155653b2c8f8888e4067477
pascal <pascal.nicintije@student.moringaschool.com>
2024-03-18 15:38:48 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 45;
+let num4 =41;
+let mod = num3 % num4
+
+let numbers = [16, 5, 15, 4, 14, 20];
+let max = Math.max( ...numbers)
